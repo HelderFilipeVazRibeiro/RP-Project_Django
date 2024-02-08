0f07e89cadbe226fb4bbe03f1231f9ad32fa4017
Helder Ribeiro <helder.ribeiro@orangedynamic.pt>
2024-02-08 16:49:02 +0000

    test

--- pages/urls.py
+++ pages/urls.py
@@ -5,4 +5,7 @@ from pages import views
 
 urlpatterns = [
     path("", views.home, name='home'),
+    path("home2", views.home2, name='home2'),
+    path("login", views.login, name='login'),
+    path("register", views.register, name='register'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -4,3 +4,13 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, "pages/home.html", {})
+
+def home2(request):
+    return render(request, "pages/home2.html", {})
+
+def login(request):
+    return render(request, "pages/login.html", {} )
+
+def register(request):
+    return render(request, "pages/register.html", {} )
+
